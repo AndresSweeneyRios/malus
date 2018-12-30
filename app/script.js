ffc97fdaa5ac3cb4be75e7c2c1c2d9beb82e2527
Andr3wRiv3rs <andrewarivers@gmail.com>
2018-12-30 13:46:35 -0800

    App Initialized

---
+++ app/script.js
@@ -0,0 +1,1 @@
+const mapper = require('nat-upnp-wrapper')
---
+++ main.js
@@ -0,0 +1,15 @@
+const {app, BrowserWindow} = require('electron')
+let mainWindow
+
+process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = true
+
+const createWindow = () => {
+  mainWindow = new BrowserWindow({width: 1200, height: 800})
+  mainWindow.loadFile('./app/index.html')
+  mainWindow.webContents.openDevTools()
+  mainWindow.on('closed', () => mainWindow = null)
+}
+
+app.on('ready', createWindow)
+app.on('window-all-closed', () => process.platform !== 'darwin' ? app.quit() : '')
+app.on('activate', () => mainWindow === null ? createWindow() : '')
