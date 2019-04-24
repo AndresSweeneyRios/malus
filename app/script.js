f7c2b01277d9061d1a23b39be065bac5022f2c7a
Andr3wRiv3rs <andrewarivers@gmail.com>
2019-04-23 21:26:32 -0700

    Pre release style fixed
    Fixed the Add Port modal and input

--- app/script.js
+++ app/script.js
@@ -1,6 +1,4 @@
-const fs = require('fs')
-
 const components = {}
 
-for (const component of fs.readdirSync('app/components'))
-    components[component.replace('.js','')] = new Vue(require('./components/' + component))
+for (const component of ['main', 'title-bar'])
+    components[component] = new Vue(require('./components/' + component))
